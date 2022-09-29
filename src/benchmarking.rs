c16309d36b759c54947b002d9def6265ff1b55ef
jaypan <pan@peaq.io>
2022-09-29 16:08:21 +0200

    Add new weight

--- src/benchmarking.rs
+++ src/benchmarking.rs
@@ -1,20 +1,67 @@
-//! Benchmarking setup for peaq-pallet-transaction
+#![cfg(feature = "runtime-benchmarks")]
 
 use super::*;
 
-#[allow(unused)]
-use crate::Pallet as Transaction;
+use crate::Pallet as TransactionPallet;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
-use frame_system::RawOrigin;
+use frame_system::{Pallet as System, RawOrigin};
+use crate::structs::*;
+use sp_io::hashing::blake2_256;
+use frame_support::sp_runtime::traits::Hash;
 
-benchmarks! {
-	do_something {
-		let s in 0 .. 100;
-		let caller: T::AccountId = whitelisted_caller();
-	}: _(RawOrigin::Signed(caller), s)
-	verify {
-		assert_eq!(Something::<T>::get(), Some(s));
-	}
+/// Assert that the last event equals the provided one.
+fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
+    System::<T>::assert_last_event(generic_event.into());
 }
 
-impl_benchmark_test_suite!(Transaction, crate::mock::new_test_ext(), crate::mock::Test);
+benchmarks! {
+    service_requested {
+        let token_deposited = BalanceOf::<T>::from(100_000u32);
+        let provider: T::AccountId = whitelisted_caller();
+        let caller = whitelisted_caller();
+    }: _(RawOrigin::Signed(caller), provider.clone(), token_deposited)
+    verify {
+        assert_last_event::<T>(Event::<T>::ServiceRequested {
+            consumer: whitelisted_caller(),
+            provider,
+            token_deposited
+        }.into());
+    }
+
+    service_delivered {
+        let provider: T::AccountId = whitelisted_caller();
+        let caller = whitelisted_caller();
+
+		let info = DeliveredInfo::<BalanceOf::<T>, T::Hash, T::BlockNumber> {
+			token_num: BalanceOf::<T>::from(25u32),
+			tx_hash: T::Hashing::hash_of(&blake2_256(b"tx hash")),
+			time_point: TransactionPallet::<T>::now(),
+			call_hash: blake2_256(b"call hash"),
+		};
+
+    }: _(RawOrigin::Signed(caller), provider.clone(), info.clone(), info.clone())
+    verify {
+        assert_last_event::<T>(Event::<T>::ServiceDelivered {
+            consumer: whitelisted_caller(),
+            provider: whitelisted_caller(),
+            refund_info: info.clone(),
+            spent_info: info.clone(),
+        }.into());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::mock;
+    use frame_support::sp_io::TestExternalities;
+
+    pub fn new_test_ext() -> TestExternalities {
+        mock::ExternalityBuilder::build()
+    }
+}
+
+impl_benchmark_test_suite!(
+    Pallet,
+    crate::benchmarking::tests::new_test_ext(),
+    crate::mock::TestRuntime,
+);
