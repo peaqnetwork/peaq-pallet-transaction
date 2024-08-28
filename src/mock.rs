6a264b5fe6e11f9453feb4432d7eb964f04dd777
jaypan <pan@peaq.io>
2024-08-28 18:06:34 +0200

    Fix fmt

--- src/mock.rs
+++ src/mock.rs
@@ -47,7 +47,7 @@ impl system::Config for Test {
     type SS58Prefix = SS58Prefix;
     type OnSetCode = ();
     type MaxConsumers = frame_support::traits::ConstU32<16>;
-	type RuntimeTask = ();
+    type RuntimeTask = ();
 }
 
 parameter_types! {
@@ -68,7 +68,7 @@ impl pallet_balances::Config for Test {
     // type MaxHolds = ();
     type MaxFreezes = ();
     type RuntimeHoldReason = ();
-	type RuntimeFreezeReason = ();
+    type RuntimeFreezeReason = ();
 }
 
 impl peaq_pallet_transaction::Config for Test {
